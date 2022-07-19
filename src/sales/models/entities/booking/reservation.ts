bafbf234b26301b0b6c6146ea14411345b5375a9
Bruno Trevisan <bru.trevz@gmail.com>
2022-07-19 12:44:45 -0300

    chore: add reservation enums to entity

--- src/sales/models/entities/booking/reservation.ts
+++ src/sales/models/entities/booking/reservation.ts
@@ -1,7 +1,11 @@
+import { ReservationStatus, ReservationType } from '@/sales/models/enums'
+
 export abstract class Reservation {
   constructor(
     public providerId: string,
     public contractCode: string,
+    public type: ReservationType,
+    public status: ReservationStatus,
     public reservationReference?: string,
     public cancellationReference?: string
   ) {}

eea55139f38a2803fe957bfdf8a55d066189054a
Bruno Trevisan <bru.trevz@gmail.com>
2022-07-20 14:26:17 -0300

    chore: add payment props

--- src/sales/models/entities/payment.ts
+++ src/sales/models/entities/payment.ts
@@ -1,1 +1,8 @@
-export class Payment {}
+export class Payment {
+  constructor(
+    public id: string,
+    public confirmationReference: number,
+    public cancelationReference: number,
+    public processedDate: Date
+  ) {}
+}

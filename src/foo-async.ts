38ee5739d3d9587278022a27a102d1ce8df58dee
Bruno Trevisan <bru.trevz@gmail.com>
2022-07-21 12:51:43 -0300

    chore: removing placeholder files

--- src/foo-async.ts
+++
@@ -1,7 +0,0 @@
-export const fooAsync = async (): Promise<string> => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('foo')
-    }, 50)
-  })
-}
--- tests/asynchronous.spec.ts
+++
@@ -1,8 +0,0 @@
-import { fooAsync } from '@/foo-async'
-
-describe('asynchronous-test-suite', () => {
-  it('should resolve async call', async () => {
-    var res = await fooAsync()
-    expect(res).toBe('foo')
-  })
-})

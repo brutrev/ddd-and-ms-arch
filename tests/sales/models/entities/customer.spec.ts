c2ef05f9519f76fc86b8a1aa38ccaf1fd87dc7fd
Bruno Trevisan <bru.trevz@gmail.com>
2022-07-21 12:48:11 -0300

    fix: customer test

--- tests/sales/models/entities/customer.spec.ts
+++ tests/sales/models/entities/customer.spec.ts
@@ -5,7 +5,17 @@ describe('Customer', () => {
   it('should create an instance', () => {
     const address = new Address('123 Main St', '', 'Anytown', 'CA', '12345')
 
-    const customer = new Customer(1, 'John', '', 'Doe', address)
+    const customer = new Customer(
+      1,
+      'John',
+      '',
+      'Doe',
+      address,
+      'foo@foo.com',
+      '123-456-7890',
+      new Date(1990, 1, 1),
+      new Date()
+    )
     expect(customer).toBeTruthy()
   })
 })
